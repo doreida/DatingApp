d5b3d1f228e20c304e4170f6ee364dc452c2302d
doreidav <doreida.veizaj@sparklab.al>
2023-10-20 11:43:19 +0200

    adding photoUrl

--- API/DTOs/MemberDTO.cs
+++ API/DTOs/MemberDTO.cs
@@ -4,6 +4,7 @@ public class MemberDTO
 {
    public int Id { get; set; }
    public string UserName{get; set;}
+   public string PhotoUrl{get; set;}
    public int Age { get; set; }
    public string KnownAs { get; set; }
    public DateTime Created { get; set; }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,9 @@ public class AutoMapperProfiles: Profile
 {
   public AutoMapperProfiles()
   {
-    CreateMap<AppUser,MemberDTO>();
+    CreateMap<AppUser,MemberDTO>()
+        .ForMember(dest => dest.PhotoUrl,opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x=>x.IsMain).Url));
     CreateMap<Photo,PhotoDTO>();
+
   }
 }
