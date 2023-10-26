ebe6e0872c2816937cc7c1dd027f34aa5fba8ef5
doreidav <doreida.veizaj@sparklab.al>
2023-10-26 16:04:45 +0200

    adding loading indicator

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,8 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS, useClass: ErrorsInterceptor, multi: true},
-    {provide:HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    {provide:HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide:HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
