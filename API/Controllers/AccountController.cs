13d80b157dedc6cbd0f7dfca686038266eb311a0
doreidav <doreida.veizaj@sparklab.al>
2023-10-30 16:44:41 +0100

    setting the main photo in the client and server side

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,7 +44,9 @@ public class AccountController : BaseApiController
   [HttpPost("login")]
   public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
   {
-    var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+    var user = await _context.Users
+    .Include(p=>p.Photos)
+    .SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
 
     if (user == null) return Unauthorized("The username is invalid. Please try again!");
 
@@ -59,7 +61,8 @@ public class AccountController : BaseApiController
 
     return new UserDTO{
       Username = user.UserName,
-      Token = _tokenService.CreateToken(user)
+      Token = _tokenService.CreateToken(user),
+      PhotoUrl = user.Photos.FirstOrDefault(x=> x.IsMain)?.Url
     };
   }
 
