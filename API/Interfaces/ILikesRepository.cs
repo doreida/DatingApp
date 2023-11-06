9e161d426ab9b531e7bb164c17c35d5853bdc019
doreidav <doreida.veizaj@sparklab.al>
2023-11-06 12:49:39 +0100

    adding and implementing LikeSRepository

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,11 @@
+﻿using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+   Task<UserLike> GetUserLike (int sourceUserId, int targetUserId);
+   Task<AppUser> GetUserWithLikes(int userId);
+   Task<IEnumerable<LikeDTO>> GetUserLikes(string predicate, int userId);
+}
