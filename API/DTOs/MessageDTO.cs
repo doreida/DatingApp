e532ef8fa6afd32fdf8d0f92a5f742906a77542a
doreidav <doreida.veizaj@sparklab.al>
2023-11-07 16:53:02 +0100

    Setting up the automapper profiles

--- API/DTOs/MessageDTO.cs
+++ API/DTOs/MessageDTO.cs
@@ -8,7 +8,7 @@ public class MessageDTO
    public string SenderPhotoUrl { get; set; }
    public int RecipientId { get; set; }
    public string RecipientUsername { get; set; }
-   public string PecipientPhotoUrl { get; set; }
+   public string RecipientPhotoUrl { get; set; }
    public string Content { get; set; }
    public DateTime? DateRead { get; set; }
    public DateTime MessageSent { get; set; }
