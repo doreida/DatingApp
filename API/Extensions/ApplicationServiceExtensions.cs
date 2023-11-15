e45243c38de25ed051e218d66850f4fade216ccf
doreidav <doreida.veizaj@sparklab.al>
2023-11-15 12:09:40 +0100

    Adding a presence tracker

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,7 @@ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -26,6 +27,8 @@ public static class ApplicationServiceExtensions
       services.AddScoped<ILikesRepository, LikesRepository>();
       services.AddScoped<IMessageRepository, MessageRepository>();
       services.AddSignalR();
+      services.AddSingleton<PresenceTracker>();
+
       return services;
     }
  
